db1607965e146ad67796e89937806e133d47403f
leoziondev <leozionsuporte@gmail.com>
2022-04-10 13:37:06 -0300

    add user controller and routes

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::get();
+
+        return view('users.index', compact('users'));
+    }
+
+    public function show($id)
+    {
+        // $user = User::where('id', $id)->first();
+        if (!$user = User::find($id))
+            return redirect()->route('users.index');
+
+        return view('users.show', compact('user'));
+    }
+}
