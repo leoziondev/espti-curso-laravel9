6179ca033ac0343ca281eca4835b4d0d00847bfb
leoziondev <leozionsuporte@gmail.com>
2022-04-11 16:26:18 -0300

    edit and update comment

--- resources/views/users/comments/_partials/form.blade.php
+++ resources/views/users/comments/_partials/form.blade.php
@@ -1,9 +1,13 @@
 <div class="w-full bg-white rounded px-8 py-12 mt-4">
   @csrf
 
-  <textarea name="body" id="body" rows="5" class="bg-gray-50 appearance-none border border-gray-100 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none my-2" placeholder="Comentário"></textarea>
+  <textarea name="body" id="body" rows="5" class="bg-gray-50 appearance-none border border-gray-100 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none my-2" placeholder="Comentário">{{ $comment->body ?? old('body') }}</textarea>
   <label for="visible">
-    <input type="checkbox" name="visible">
+    <input type="checkbox" name="visible" 
+      @if (isset($comment) && $comment->visible)
+        checked="checked"
+      @endif
+    >
     Visível?
   </label>
   
--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,10 @@ use App\Http\Controllers\{
 };
 use App\Http\Controllers\Admin\CommentController;
 
-Route::post('/users/{id}/comments', [CommentController::class, 'store'])->name('comments.store');
 Route::get('/users/{id}/comments/create', [CommentController::class, 'create'])->name('comments.create');
+Route::get('/users/{userId}/comments/{id}', [CommentController::class, 'edit'])->name('comments.edit');
+Route::put('/comments/{id}', [CommentController::class, 'update'])->name('comments.update');
+Route::post('/users/{id}/comments', [CommentController::class, 'store'])->name('comments.store');
 Route::get('/users/{id}/comments', [CommentController::class, 'index'])->name('comments.index');
 
 Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
